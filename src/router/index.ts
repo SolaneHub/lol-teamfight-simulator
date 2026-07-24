bec4e824cef721dd85f49785c2c363de18bd7c9d
Solane <simonelostimolo@gmail.com>
2026-07-24 19:33:47 +0200

    fix: switch to hash router history mode for GitHub Pages SPA compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BrowserView from '../views/BrowserView.vue'
 import CustomizerView from '../views/CustomizerView.vue'
 import CalculatorView from '../views/CalculatorView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
